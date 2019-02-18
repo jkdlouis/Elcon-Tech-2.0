e037d78a55d7d8de3a73e06b8d8ec1d5c16d88ae
Louis Huang <lhuang5@apple.com>
2019-02-17 16:37:32 -0800

    refactor and simplify timeLine component for home page.

--- src/components/UI/Timeline/Timeline.js
+++ src/components/UI/Timeline/Timeline.js
@@ -6,45 +6,31 @@ import './TImeline.scss';
 
 const timeLine = (props) => {
         let timeLine = <Spinner/>;
-        let verticalTimelineElement = null;
+        let verticalTimeLineElement = null;
 
         if (props.timeLineData && props.timeLineData.data.length) {
             timeLine = props.timeLineData.data.map((data, index) => {
                 index += 1;
-                if (index % 2 === 0) {
-                    verticalTimelineElement = (
+                    verticalTimeLineElement = (
                         <VerticalTimelineElement
-                            className="vertical-timeline-element--education"
+                            className={ index % 2 === 0 ? "vertical-timeline-element--education" : "vertical-timeline-element--work odd" }
                             date={ data.year }
-                            position="right"
-                            iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-                        >
+                            position={ index % 2 === 0 ? "right" : "left" }
+                            iconStyle={ index % 2 === 0 ?
+                                { background: 'rgb(233, 30, 99)', color: '#fff' }
+                                :  { background: 'rgb(33, 150, 243)', color: '#fff' }
+                            }>
                             <h3 className="vertical-timeline-element-title">{ data.title }</h3>
                             <h4 className="vertical-timeline-element-subtitle">{ data.location }</h4>
                             <p>
                                 { data.description }
                             </p>
                         </VerticalTimelineElement>
-                    )
-                } else {
-                    verticalTimelineElement = (
-                        <VerticalTimelineElement
-                            className="vertical-timeline-element--work odd"
-                            date={ data.year }
-                            position="left"
-                            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                        >
-                            <h3 className="vertical-timeline-element-title">{ data.title }</h3>
-                            <h4 className="vertical-timeline-element-subtitle">{ data.location }</h4>
-                            <p>
-                                { data.description }
-                            </p>
-                        </VerticalTimelineElement>
-                    )
-                }
+                    );
+
                     return (
                         <VerticalTimeline key={ data.year }>
-                            { verticalTimelineElement }
+                            { verticalTimeLineElement }
                         </VerticalTimeline>
                     )
                 }
@@ -53,7 +39,7 @@ const timeLine = (props) => {
 
         return (
             <Fragment>
-                {timeLine}
+                { timeLine }
             </Fragment>
         )
     }
